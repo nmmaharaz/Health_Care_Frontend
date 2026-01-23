a0f7b58d8797fdda4533e91fa6439e1146afcf6c
nmmaharaz <nmmaharaz@gmail.com>
2026-01-24 01:28:19 +0600

    add RefreshButton

---
+++ components/shared/RefreshButton.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { RefreshCcw } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useTransition } from "react"
+
+interface RefreshButtonProps {
+    className: string,
+    showLabel: string
+}
+
+export default function RefreshButton({ showLabel, className }: RefreshButtonProps) {
+    const router = useRouter()
+    const [isPending, setTransition] = useTransition()
+    const handleRefresh = () => {
+        setTransition(() => {
+            router.refresh()
+        })
+    }
+    return (
+        <button
+            className={className}
+            onClick={handleRefresh}
+        >
+            <RefreshCcw
+                className={`h-4 w-4 ${isPending ? "animate-spin" : ""} ${showLabel ? "mr-2" : ""
+                    }`}
+            />
+        </button>
+    )
+}
